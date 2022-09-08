49c5b25a32200b27096e19eb9e27ab226991a06e
PsionicGeek <rrkumar220920@gmail.com>
2022-09-08 09:45:05 +0530

    MaximumOfAllSubarraysOfSizeK

--- README.md
+++ README.md
@@ -15,7 +15,16 @@ Must Do Coding Questions for Companies like Amazon, Microsoft, Adobe
  - [Rearrange array Alternatively](https://practice.geeksforgeeks.org/problems/-rearrange-array-alternately-1587115620/1)
  - [Number of Pairs](https://practice.geeksforgeeks.org/problems/number-of-pairs-1587115620/1)
  - [Count Inversions](https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1)
-
+ - [Sort an Array of 0's 1's and 2's](https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1)
+ - [Equillibrium Point](https://practice.geeksforgeeks.org/problems/equilibrium-point-1587115620/1)
+ - [Leaders in an Array](https://practice.geeksforgeeks.org/problems/leaders-in-an-array-1587115620/1)
+ - [Minimum Platform](https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1)
+ - [Reverse array in groups](https://practice.geeksforgeeks.org/problems/reverse-array-in-groups0255/1)
+ - [K'th Smallest Element](https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1)
+ - [Trapping Rainwater](https://practice.geeksforgeeks.org/problems/trapping-rain-water-1587115621/1)
+ - [Pythagorean Triplet](https://practice.geeksforgeeks.org/problems/pythagorean-triplet3018/1)
+ - [Chocolate Distribution Problem](https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1)
+ - [Maximum of all subarrays of size k](https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1)
 
 ## Author
 
---
+++ src/MaximumOfAllSubarraysOfSizeK.java
@@ -0,0 +1,119 @@
+//https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
+//Given an array arr[] of size N and an integer K. Find the maximum for each and every contiguous subarray of size K.
+//Example:-
+//Input:
+//N = 9, K = 3
+//arr[] = 1 2 3 1 4 5 2 3 6
+//Output:
+//3 3 4 5 5 5 6
+//Explanation:
+//1st contiguous subarray = {1 2 3} Max = 3
+//2nd contiguous subarray = {2 3 1} Max = 3
+//3rd contiguous subarray = {3 1 4} Max = 4
+//4th contiguous subarray = {1 4 5} Max = 5
+//5th contiguous subarray = {4 5 2} Max = 5
+//6th contiguous subarray = {5 2 3} Max = 5
+//7th contiguous subarray = {2 3 6} Max = 6
+
+import java.util.*;
+public class MaximumOfAllSubarraysOfSizeK {
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //taking total count of testcases
+        int t = sc.nextInt();
+
+
+
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+
+            //taking size of subArrays
+            int k = sc.nextInt();
+
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+
+            //adding all the elements to the array
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new MaximumOfAllSubarraysOfSizeK().max_of_subarrays(arr, n, k);
+
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+    static ArrayList <Integer> max_of_subarrays(int nums[], int n, int k)
+    {
+        ArrayList<Integer> result=new ArrayList<>();
+        if(nums.length==1||k==1){
+            for(int i:nums){
+                result.add(i);
+            }
+            return result;
+        }
+
+        int max1=nums[0];
+        int in1=0;
+        int in2=0;
+        int max2=nums[1];
+        for(int i=1;i<k;i++){
+            if(nums[i]>=max1){
+                in2=in1;
+                max2=max1;
+                in1=i;
+                max1=nums[i];
+            }
+        }
+        result.add(max1);
+        int start=1;
+        int end=k;
+        while(end<nums.length){
+            if(nums[end]>=max1){
+                max2=max1;
+                in2=in1;
+                max1=nums[end];
+                in1=end;
+                end++;
+                result.add(max1);
+
+            }
+            else{
+                if(nums[end]>=max2){
+                    max2=nums[end];
+                    in2=end;
+                }
+
+                if(in1<=end-k||in2<=end-k){
+                    max1=nums[end-k+1];
+
+                    for(int i=end-k+1;i<=end;i++){
+                        if(nums[i]>=max1){
+                            in2=in1;
+                            max2=max1;
+                            in1=i;
+                            max1=nums[i];
+                        }
+                    }
+                }
+
+
+                end++;
+                result.add(max1);
+
+            }
+        }
+        return result;
+    }
+}
